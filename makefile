40a7cf8451c96a578d327687c8f5f1b74619f5fe
Blezzing <mmhmaster@hotmail.com>
2017-02-03 03:32:10 +0100

    major logic rewrite, new data structure and configuration

--- makefile
+++ makefile
@@ -1,2 +1,5 @@
 install: ./src/main.c
 	gcc -Wall -g ./src/main.c -o ./bin/blezz
+
+run: install
+	./bin/blezz
--- src/main.c
+++ src/main.c
@@ -2,40 +2,40 @@
 #include<stdlib.h>
 #include<string.h>
 
-#define DEFAULT_CONFIG_PATH "/home/blezzing/Git/blezz/cfg/blezzrc"
+#include"main.h"
 
-typedef struct Menu Menu;
-typedef struct Action Action;
-typedef struct Item Item;
+#define DEFAULT_CONFIG_PATH "/home/blezzing/Git/blezz/cfg/blezzrc2"
+#define NO_KEY '\0'
+#define LEVEL_UP_KEY '-'
+#define DIR_SYMBOL '>'
+#define ACT_SYMBOL '!'
 
-struct Menu{
-	char* title;
-    Item* link;
-};
+typedef struct Dir Dir;
+typedef struct Act Act;
 
-struct Action{
-	char* command;
-    Item* link;
-};
-
-struct Item{
-	char key;
+struct Dir{
+    char key;
 	char* label;
-    Menu* menu;
+    Dir* parent;
 };
 
-int allMenusAllocationSize = 10;
-int allMenusCount = 0;
-Menu** allMenus = NULL;
+struct Act{
+    char key;
+    char* label;
+	char* command;
+    Dir* parent;
+};
 
-int allActionsAllocationSize = 10;
-int allActionsCount = 0;
-Action** allActions = NULL;
+//used to calculate maximum allocation sizes to avoid reallocs
+int configLines = 0;
 
-int allItemsAllocationSize = 10;
-int allItemsCount = 0;
-Item** allItems = NULL;
+int savedDirs = 0;
+int savedActs = 0;
+Dir** allDirs = NULL;
+Act** allActs = NULL;
 
+int dirStackTop = -1;
+Dir** dirStack;
 
 void memError(){
     fprintf(stderr,"Out of memory.\n");
@@ -47,52 +47,95 @@ void fileError(){
     exit(2);
 }
 
-void addMenu(Menu* menu) {
-    if (allMenus==NULL) {
-        allMenus=(Menu**)malloc(sizeof(Menu*)*allMenusAllocationSize);
-        if (allMenus==NULL) {
-            memError();
-        }
-    }
-    
-    if (allMenusCount + 1 >= allMenusAllocationSize) {
-        allMenusAllocationSize = allMenusAllocationSize * 2;
-        allMenus=(Menu**)realloc(allMenus,sizeof(Menu*)*allMenusAllocationSize);
-    }
-    
-    allMenus[allMenusCount++] = menu;
+int isDirRef(char* string){
+    return strlen(string)>6 && (strncmp(string,"dir(",4) == 0);
 }
 
-void addAction(Action* action) {
-    if (allActions==NULL) {
-        allActions=(Action**)malloc(sizeof(Action*)*allActionsAllocationSize);
-        if (allActions==NULL) {
-            memError();
-        }
-    }
-    
-    if (allActionsCount + 1 >= allActionsAllocationSize) {
-        allActionsAllocationSize = allActionsAllocationSize * 2;
-        allActions=(Action**)realloc(allActions,sizeof(Action*)*allActionsAllocationSize);
-    }
-    
-    allActions[allActionsCount++] = action;
+int isActRef(char* string){
+    return strlen(string)>8 && (strncmp(string,"act(",4) == 0);
 }
 
-void addItem(Item* item) {
-    if (allItems==NULL) {
-        allItems=(Item**)malloc(sizeof(Item*)*allItemsAllocationSize);
-        if (allItems==NULL) {
-            memError();
+int isDirDecl(char* string){
+    return (strcspn(string,":") != strlen(string));
+}
+
+Dir* newDirFromRef(char* string, Dir* parent){
+    int keyIndex = 4;
+    int labelIndex = 6;
+    int labelLength = strlen(string)-(labelIndex+1);
+    
+    Dir* dir = (Dir*)malloc(sizeof(Dir));
+    dir->key = string[keyIndex];
+    dir->label = (char*)malloc(sizeof(char)*(labelLength+1));
+    strncpy(dir->label,string+labelIndex,labelLength);
+    dir->label[labelLength]='\0';
+    dir->parent = parent;
+    
+    return dir;
+}
+
+Dir* newDirFromDecl(char* string){
+    int colonIndex = strcspn(string,":");
+    
+    Dir* dir = (Dir*)malloc(sizeof(Dir));
+    dir->key = NO_KEY;
+    dir->label = (char*)malloc(sizeof(char)*(colonIndex+1));
+    strncpy(dir->label,string,colonIndex);
+    dir->label[colonIndex]='\0';
+    dir->parent = NULL;
+    
+    return dir;
+}
+
+Act* newActFromRef(char* string, Dir* parent){
+    int keyIndex = 4;
+    int labelIndex = 6;
+    int labelLength = strcspn(string+labelIndex,",");
+    int commandIndex = labelIndex + labelLength+1;
+    int commandLength = strcspn(string+commandIndex,")");
+    
+    Act* act = (Act*)malloc(sizeof(Act));
+    act->key = string[keyIndex];
+    act->label = (char*)malloc(sizeof(char)*(labelLength+1));
+    strncpy(act->label,string+labelIndex,labelLength);
+    act->label[labelLength] = '\0';
+    act->command = (char*)malloc(sizeof(char)*(commandLength+1));
+    strncpy(act->command,string+commandIndex,commandLength);
+    act->command[commandLength] = '\0';
+    act->parent = parent;
+    
+    return act;
+}
+
+Dir* addDir(Dir* dir){
+    if (allDirs==NULL) {
+        allDirs = (Dir**)malloc(sizeof(Dir*)*configLines);
+    }
+    
+    for (int d = 0; d < savedDirs; d++) {
+        if (strcmp(allDirs[d]->label,dir->label) == 0) {
+            if (allDirs[d]->parent == NULL) {
+                allDirs[d]->parent = dir->parent;
+            }
+            if (allDirs[d]->key == NO_KEY) {
+                allDirs[d]->key = dir->key;
+            }
+            free(dir->label);
+            free(dir);
+            
+            return allDirs[d];
         }
     }
     
-    if (allItemsCount + 1 >= allItemsAllocationSize) {
-        allItemsAllocationSize = allItemsAllocationSize * 2;
-        allItems=(Item**)realloc(allItems,sizeof(Item*)*allItemsAllocationSize);
+    return allDirs[savedDirs++] = dir;
+}
+
+Act* addAct(Act* act){
+    if (allActs==NULL) {
+        allActs = (Act**)malloc(sizeof(Act*)*configLines);
     }
     
-    allItems[allItemsCount++] = item;
+    return allActs[savedActs++] = act;
 }
 
 char** getLines(FILE* file) {
@@ -122,7 +165,7 @@ char** getLines(FILE* file) {
         
         //allocate line, and make sure we succeed
         lines[i] = malloc(line_size_to_allocate);
-        if(lines==NULL) {
+        if (lines[i]==NULL) {
             memError();
         }
         
@@ -138,176 +181,141 @@ char** getLines(FILE* file) {
         lines[i][j+1]='\0';
     }
     
+    configLines = sizeof(lines)/sizeof(char*);
     return lines;   
 }
 
-int isMenu(char* string) {
-    return (strlen(string) > 5 && strncmp(string,"menu:",5) == 0);
-}
-
-int isAction(char* string) {
-    return (strlen(string) > 8 && strncmp(string,"action:",7) == 0);
-}
-
-int isItem(char* string) {
-    return (strlen(string) > 8 && string[1] == ':' && string[strlen(string)-1] == ')' && strcspn(string,"(") != strlen(string));
-}
-
-Menu* newMenu(char* string, Item* item) {
-    if (isMenu(string)) {
-        char* mTitle = (char*)malloc(sizeof(char)*(strlen(string)-5));
-        strncpy(mTitle,string+5,strlen(string)-4);
-        
-        for (int i = 0; i < allMenusCount; i++)
-        {
-            if (strcmp(allMenus[i]->title,mTitle)==0) {
-                if(allMenus[i]->link==NULL) {
-                    allMenus[i]->link = item;
-                }
-                
-                free(mTitle);
-                return allMenus[i];
-            }
-        }
-        
-        Menu* menu = (Menu*)malloc(sizeof(Menu));
-        menu->title = mTitle;
-        menu->link = item;
-        
-        addMenu(menu);
-        
-        return menu;
-    }
-
-    return NULL;
-}
-
-Action* newAction(char* string, Item* item) {
-    if (isAction(string)) {
-        char* aCommand = (char*)malloc(sizeof(char)*(strlen(string)-6));
-        strncpy(aCommand,string+7,strlen(string)-6);
-        aCommand[strlen(string)-7] = '\0';
-        
-        Action* action = (Action*)malloc(sizeof(Action));
-        action->command = aCommand;
-        action->link = item;
-        
-        addAction(action);
-        
-        return action;
-    }
-    return NULL;
-}
-
-Item* newItem(char* string, Menu* iMenu) {
-    if (isItem(string) && !(iMenu == NULL)) {
-        char iKey = string[0];
-        int parIndex = strcspn(string,"(");
-        char* iLabel = (char*)malloc(sizeof(char)*(parIndex-3));
-        strncpy(iLabel,string+2,parIndex-2);
-        iLabel[parIndex-2]='\0';
-        
-        char* content = (char*)malloc(sizeof(char)*(strlen(string) - (parIndex)));
-        strncpy(content,string+parIndex+1,(strlen(string) - (parIndex + 2)));
-        content[(strlen(string) - (parIndex + 2))]='\0';
-        
-        Item* item = (Item*)malloc(sizeof(Item));
-        item->key = iKey;
-        item->menu = iMenu;
-        item->label = iLabel;
-        
-        addItem(item);
-        
-        if (isAction(content)) {
-            newAction(content,item);
-        }
-        
-        if (isMenu(content)) {
-            newMenu(content,item);
-        }
-        
-        return item;
-    }
-    
-    return NULL;
-}
-
-Menu* buildMenuFromFile(char* path) {
+Dir* importStructure(char* path) {
     FILE* file;
     char** lines;
-    Menu* rootMenu = NULL;
+    Dir* root = NULL;
     
-    printf("%s %s","Loading structure from:",path); 
-    file = fopen(path,"r"); //TODO: add some sort of errorhandling here, missing folders give segfault
+    printf("%s %s\n","Loading structure from:",path); 
+    file = fopen(path,"r"); //TODO: add errorhandling here, missing folders give segfault
     lines = getLines(file);
     fclose(file);
-
-    //type checking happens in constructors
-    Menu* currMenu = NULL;
+    
+    Dir* currDir = NULL;
     for (int i = 0; lines[i] != NULL; i++) {
-        Menu* menu = newMenu(lines[i],NULL);
-        if (!(menu==NULL)) {
-            if(currMenu==NULL) {
-                rootMenu=menu;
+        if (isDirDecl(lines[i])) {
+            Dir* dir = newDirFromDecl(lines[i]);
+            dir = addDir(dir); //dir might be replaced with an already existing dir
+            currDir = dir;
+            if(root==NULL) {
+                root = dir;
             }
-            
-            currMenu=menu;
-            continue;
         }
-        
-        newItem(lines[i],currMenu);
+        else if (isDirRef(lines[i])) {
+            Dir* dir = newDirFromRef(lines[i],currDir);
+            addDir(dir);
+        }
+        else if (isActRef(lines[i])) {
+            Act* act = newActFromRef(lines[i],currDir);
+            addAct(act);
+        }        
     }
     
-    printf("%s %i\n%s %i\n%s %i\n","Items:", allItemsCount, "Menus:", allMenusCount, "Actions:", allActionsCount);
+    printf("%s %i\n%s %i\n","Dirs:", savedDirs, "Acts:", savedActs);
 
-	return rootMenu;
+	return root;
 }
 
-void printMenu(Menu* thisMenu) {
-	if (thisMenu == NULL) {
-		printf("Empty menu...");
+void printDir(Dir* dir) {
+	if (dir == NULL) {
+		printf("Empty...");
 		return;
 	}
 	
-	printf("%s:\n",thisMenu->title);
-    for(int i = 0; i < allItemsCount; i++) {
-        if ((allItems[i]->menu) == thisMenu) {
-            printf("[%c] %s\n",allItems[i]->key, allItems[i]->label);
+	printf("%s:\n",dir->label);
+    
+    for(int i = 0; i < savedDirs; i++) {
+        if (allDirs[i]->parent == dir) {
+            printf("%c[%c] %s\n",DIR_SYMBOL,allDirs[i]->key, allDirs[i]->label);
         }
+    }
+    
+    for(int i = 0; i < savedActs; i++) {
+        if (allActs[i]->parent == dir) {
+            printf("%c[%c] %s\n",ACT_SYMBOL,allActs[i]->key, allActs[i]->label);
+        }
+    }            
+}
+
+void dirStackPop() {
+    dirStackTop--;
+}
+
+void dirStackPush(Dir* dir) {
+    dirStackTop++;
+    dirStack[dirStackTop] = dir;
+}
+
+Dir* dirStackPeek() {
+    return dirStack[dirStackTop];
+}
+
+int dirStackIsEmpty() {
+    return dirStackTop < 0;
+}
+
+void dirStackAlloc() {
+    dirStack = (Dir**)malloc(sizeof(Dir*)*savedDirs);
+}
+
+int selectElement(Dir* dir, char choice) {
+    if(choice == LEVEL_UP_KEY) {
+        dirStackPop();
+        return dirStackIsEmpty();
+    }
+    
+    for(int i = 0; i < savedDirs; i++) {
+        if(allDirs[i]->parent == dir && allDirs[i]->key == choice){
+            dirStackPush(allDirs[i]);
+            return 0;
+        }
+    }
+    
+    for(int i = 0; i < savedActs; i++) {
+        if (allActs[i]->parent == dir && allActs[i]->key == choice){
+            system(allActs[i]->command);
+            return 1;
+        }
+    }
+    
+    return 0;
+}
+
+void inputLoop() {
+    int done = 0;
+    while(done == 0) {
+        Dir* currentDir = dirStackPeek();
+        printDir(currentDir); 
+        char c = fgetc(stdin);
+        while(fgetc(stdin) != '\n'); //purge input
+        done = selectElement(currentDir,c);
     }
 }
 
-Menu* currentMenu;
-int selectElement(Menu* inMenu, char choice) {
-    for(int i = 0; i < allItemsCount; i++) {
-        if(allItems[i]->menu == inMenu && allItems[i]->key == choice){
-            for(int m = 0; m < allMenusCount; m++) {
-                if (allMenus[m]->link == allItems[i]) {
-                    currentMenu = allMenus[m];
-                }
-            }
-            for(int a = 0; a < allActionsCount; a++) {
-                if (allActions[a]->link == allItems[i]) {
-                    system(allActions[a]->command);
-                    return 1;
-                }
-            }
-        }
-    }
-    return 0;
+void printDebug() {
+    puts("DIRS:\n");
+    for(int i=1; i< savedDirs; i++)
+        printf("%c-%s>>%s\n",allDirs[i]->key,allDirs[i]->label,allDirs[i]->parent->label);
+    
+    
+    puts("ACTS:\n");
+    for(int i=0; i< savedActs; i++)
+        printf("%c-%s->%s\n",allActs[i]->key,allActs[i]->label,allActs[i]->command);
 }
 
 int main(int argc, char *argv[]) {
 	char* path = (argc >= 2)?argv[1]:DEFAULT_CONFIG_PATH;
-    currentMenu = buildMenuFromFile(path);
+    Dir* root = importStructure(path);
     
-    int done = 0;
-    while(done == 0) {
-        printMenu(currentMenu); 
-        char c = fgetc(stdin);
-        while(fgetc(stdin) != '\n'); //purge input
-        done = selectElement(currentMenu,c);
-    }
+    dirStackAlloc();
+    dirStackPush(root);
+    
+    inputLoop();
+    //printDebug();
     
 	return 0;
 }
